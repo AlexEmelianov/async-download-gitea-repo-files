603f26864f34828e5f3c9178949441893f4776e3
Алексей Емельянов <emelyanov000@gmail.com>
2023-04-14 14:28:49 +0300

    flake8 fixes: naming, functions split, etc.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import aiohttp
 import aiofiles
 import aiofiles.os
 import os
-import requests
 import hashlib
 from bs4 import BeautifulSoup
 from collections import deque
@@ -17,38 +16,66 @@ LOCAL_URL = '/radium/project-configuration'
 BRANCH_NAME = 'master'
 FOLDER_TO_SAVE = 'temp_folder'
 NUMBER_OF_WORKERS = 3
-URLS = deque()
+QUEUE = deque()
 
 
-async def download_file(client: aiohttp.ClientSession, idx: int) -> None:
+async def analyze_and_download(client: aiohttp.ClientSession, idx: int) -> None:
     """
-    Search urls contains `/raw`.
-    Write content to the corresponding file in directory FOLDER_TO_SAVE.
+    Get page content, then fill `QUEUE` or download file.
 
     :param client: async http client
     :param idx: number of worker
     :return: None
     """
-    while URLS:
-        url = URLS.popleft()
-        text = await get_data(client=client, url=f'{BASE_URL}{url}')
+    while QUEUE:
+        url = QUEUE.popleft()
+        text = await fetch_from_url(client=client, url=f'{BASE_URL}{url}')
         soup = BeautifulSoup(text, 'html.parser')
         if soup.find('thead'):
-            find_links(soup=soup, len_source=len(url))
-            continue
-        for a_tag in soup.findAll('a'):
-            url = a_tag.get('href', '')
-            if url.startswith(f'{LOCAL_URL}/raw'):
-                path = os.path.join(FOLDER_TO_SAVE, url.split(BRANCH_NAME)[-1][1:])
-                logger.info(f'Worker_{idx} download to {path!r}')
-                text = await get_data(client=client, url=f'{BASE_URL}{url}')
-                await write_to_disk(path=path, text=text)
-                break
+            # then there is page with list of links
+            append_to_queue(soup=soup, len_source=len(url))
+        else:
+            # else there is page with one file
+            await download_file(client=client, soup=soup, idx=idx)
 
 
-async def get_data(client: aiohttp.ClientSession, url: str) -> bytes:
+def append_to_queue(soup: BeautifulSoup, len_source: int) -> None:
     """
-    Download bytestring from `url` and return it.
+    Search urls with `/src` in <a>-tags.
+
+    :param soup: html text
+    :param len_source: length of source url
+    :return: None
+    """
+    for a_tag in soup.find('tbody').findAll('a'):
+        url = a_tag.get('href', '')
+        if url.startswith(f'{LOCAL_URL}/src') and len(url) > len_source:
+            QUEUE.append(url)
+
+
+async def download_file(client: aiohttp.ClientSession, soup: BeautifulSoup, idx: int) -> None:
+    """
+    Search urls contains `/raw`.
+    Write content to the corresponding file in directory FOLDER_TO_SAVE.
+
+    :param client: async http client
+    :param soup: html text
+    :param idx: number of worker
+    :return: None
+    """
+    for a_tag in soup.findAll('a'):
+        url = a_tag.get('href', '')
+        if url.startswith(f'{LOCAL_URL}/raw'):
+            path = os.path.join(FOLDER_TO_SAVE, url.split(BRANCH_NAME)[-1][1:])
+            logger.info(f'Worker_{idx} starts downloading to {path!r}')
+            text = await fetch_from_url(client=client, url=f'{BASE_URL}{url}')
+            await write_to_disk(path=path, text=text)
+            return
+
+
+async def fetch_from_url(client: aiohttp.ClientSession, url: str) -> bytes:
+    """
+    Download bytestring from `url` and return it (3 attempts to download).
 
     :param client: async http client
     :param url: url
@@ -78,28 +105,18 @@ async def write_to_disk(path: str, text: bytes) -> None:
         await file.write(text)
 
 
-def find_links(soup: BeautifulSoup, len_source: int) -> None:
-    """
-    Search urls with `/src` in <a>-tags.
-
-    :param soup: html text
-    :param len_source: length of source url
-    :return: None
-    """
-    for a_tag in soup.find('tbody').findAll('a'):
-        url = a_tag.get('href', '')
-        if url.startswith(f'{LOCAL_URL}/src') and len(url) > len_source:
-            URLS.append(url)
-
-
 async def download_repo() -> None:
     """
-    Create async tasks that downloads files.
+    Fill `QUEUE` and create async tasks that downloads files.
 
     :return: None
     """
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(5)) as client:
-        tasks = [download_file(client, idx) for idx in range(1, NUMBER_OF_WORKERS + 1)]
+        text = await fetch_from_url(client=client, url=f'{BASE_URL}{LOCAL_URL}')
+        soup = BeautifulSoup(text, 'html.parser')
+        append_to_queue(soup=soup, len_source=len(LOCAL_URL))
+
+        tasks = [analyze_and_download(client, idx) for idx in range(1, NUMBER_OF_WORKERS + 1)]
         await asyncio.gather(*tasks)
 
 
@@ -114,8 +131,8 @@ def hash_print(folder: str) -> None:
         for filename in filenames:
             hash_obj = hashlib.sha256()
             path = os.path.join(dirpath, filename)
-            with open(path, 'rb') as file:
-                for row in file:
+            with open(path, 'rb') as file_obj:
+                for row in file_obj:
                     hash_obj.update(row)
             print(path)
             print('SHA256:', hash_obj.hexdigest())
@@ -123,16 +140,10 @@ def hash_print(folder: str) -> None:
 
 def main() -> None:
     """
-    Fill `URLS` queue by links from page `BASE_URL + LOCAL_URL`.
+    Download repository to `FOLDER_TO_SAVE`, then print hash of each file.
 
     :return:
     """
-    response = requests.get(f'{BASE_URL}{LOCAL_URL}', timeout=3)
-    if response.status_code != 200:
-        logger.error(f'Error! {response.status_code=}. Message: {response.text}')
-        return
-    soup = BeautifulSoup(response.text, 'html.parser')
-    find_links(soup=soup, len_source=len(LOCAL_URL))
     asyncio.run(download_repo())
     hash_print(FOLDER_TO_SAVE)
 
